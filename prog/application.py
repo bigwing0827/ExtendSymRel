ca5bb03d7ff8667799cb6ddf7f2f70fb008b4a0d
bigwing0827 <daiba.revolution@gmail.com>
2020-07-16 23:11:49 +0900

    ExtendSymRel

---
+++ prog/application.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+import os
+import re
+import csv
+import json
+from flask import Flask, jsonify, make_response, request, Response
+#from flask import Flask
+
+def ReadCSV(file_name):
+    list_csv = []
+    rfile = os.path.abspath(file_name)
+    with open(rfile, 'r') as f:
+        fieldnames = ['Word1', 'tag1', 'Word2', 'tag2', 'sim']
+        reader = csv.DictReader(f, fieldnames=fieldnames)
+        for row in reader:
+            list_csv.append(row)
+    return list_csv
+
+def ExtractSymRel(free_word,list_SD,ext_type):
+    ## Json_tag
+    sr_w = ext_type + '_word' ## synonym_word or relation_word
+    sr_l = ext_type + '_label' ## synonym_label or relation_label
+    sr_s = ext_type + '_similarity' ## synonym_similarity or relation_similarity
+    ## synonym/relation information
+    new_list = []
+    for entry_sd in list_SD:
+        sr_word = ""
+        sr_label = ""
+        flag = False
+        if entry_sd['Word1'] == free_word:
+            sr_word = entry_sd['Word2']
+            sr_label = entry_sd['tag2']
+            flag = True
+        if entry_sd['Word2'] == free_word:
+            sr_word = entry_sd['Word1']
+            sr_label = entry_sd['tag1']
+            flag = True
+        ## Overlap
+        if len(sr_word)!=0 and len(sr_label)!=0:
+            for i in range(len(new_list)):
+                if sr_word == new_list[i][sr_w]:
+                    flag = False
+                    break
+        if flag:
+            new_list.append({sr_w:sr_word,sr_l:sr_label,sr_s:entry_sd['sim']})
+    ## Sort (label > similarity)
+    new_list = sorted(new_list, key=lambda x:(x[sr_l],x[sr_s]), reverse=True)
+    return new_list
+
+def WordInfo(f_word,sym_set,rel_set):
+    ## free_word's label
+    if len(sym_set)!=0:
+        f_label = sym_set[0]['synonym_label']
+    else:
+        f_label = "Unknown"
+    ## free/synonym/relation information
+    one_word_info = {
+        "free_word":f_word,
+        "free_label":f_label,
+        "synonym_list":sym_set,
+        "relation_list":rel_set
+    }
+    return one_word_info
+
+
+
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+app.config["JSON_SORT_KEYS"] = False
+
+@app.route('/')
+def TopPage():
+    return "Test Page"
+
+@app.route('/synonym/extend', methods=['POST'])
+def ExtendKeyWords():
+    ## SD_dictionary
+    sym_name='../SD_data/synonym_rand.csv'
+    rel_name='../SD_data/relation_rand.csv'
+    r_list = ReadCSV(rel_name)
+    s_list = ReadCSV(sym_name)
+    ## free_word
+    #input_words = ["エミリア","音声認識","会議"]
+    #word_list = {'free_words':input_words}
+    input_words = request.get_json()
+    word_list = {'free_words': input_words['free_words']}
+    ## Search Synonym Relation
+    KW_INFO = []
+    for free_word in word_list['free_words']:
+        s_words = ExtractSymRel(free_word,s_list,'synonym')
+        r_words = ExtractSymRel(free_word,r_list,'relation')
+        fsr_set = WordInfo(free_word,s_words,r_words)
+        KW_INFO.append(fsr_set)
+    ##
+    result = {
+        "search_list":KW_INFO
+    }
+    #return result
+    #return (json.dumps(result, sort_keys=False, ensure_ascii=False, indent=2))
+    return jsonify(result) 
+
+
+
+if __name__ == "__main__":
+    #app.run()
+    app.run(debug=True, host='0.0.0.0', port=8888, threaded=True)
---
+++ prog/git_manual.md
@@ -0,0 +1,26 @@
+# Github使い方
+    - https://techacademy.jp/magazine/6235
+    
+1. ユーザ名とメアド登録
+    - $ git config --global user.name bigwing0827
+    - $ git config --global user.email daiba.revolution@gmail.com
+    - 確認 $ git config --list
+
+2. ローカルリポジトリの作成
+    - $ git init
+    - $ git add hoge.hoge
+    - $ git commit -m "ExtendSymRel"
+
+3. 確認
+    - $ git status
+
+4. リモートリポジトリの情報を追加
+    - $ git remote add origin https://github.com/bigwing0827/ExtendSymRel.git
+        - `fatal: remote origin already exists.`のとき[参考](https://qiita.com/yu-ki0718/items/3c8aae2c81ca3f82f522)
+
+5. ローカルリポジトリをプッシュしてリモートリポジトリへ反映させる（git push）
+    - $ git push origin master
+    - $ git push -u origin master
+    - `error: src refspec master does not match any`のとき[参考](https://qiita.com/kenkono/items/c488ec9559f6cca34313)
+
+6. 
